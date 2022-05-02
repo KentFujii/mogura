5e02915c6a7e3d80a498c5e924e766710c4f2db9
KentFujii <kent.where.the.light.is@gmail.com>
2022-05-02 18:35:43 +0900

    fix: suppress_push_spec_side_effects

--- spec/mogura/push_spec.rb
+++ spec/mogura/push_spec.rb
@@ -3,6 +3,9 @@ RSpec.describe Mogura::Push do
     path = File.expand_path("#{File.dirname(__FILE__)}/../fixtures")
     allow(Rails).to receive_message_chain(:root, :join, :to_s).and_return(path)
     allow(Rails).to receive_message_chain(:application, :class, :module_parent_name).and_return('mogura')
+    http = double('http')
+    allow(Net::HTTP).to receive(:start).and_yield(http)
+    expect(http).to receive(:request).once
     described_class.send(:push)
   end
 end
