4b0c0f11cfb7b7258831d2901c2fe95a712655ae
KentFujii <kent.where.the.light.is@gmail.com>
2022-09-18 20:31:15 +0900

    add: builder

---
+++ lib/mogura/builder.rb
@@ -0,0 +1,23 @@
+require 'ostruct'
+
+module Mogura
+  module Builder
+    class Dag
+      class << self
+        def build(name, content)
+          export ={
+            "_export": {
+              "rb": {
+                "require": Rails.root.join('config/environment').to_s
+              }
+            }
+          }.freeze
+          Openstruct.new(
+            name: name,
+            content: export.merge(content)
+          )
+        end
+      end
+    end
+  end
+end
--- lib/mogura/push.rb
+++ lib/mogura/push.rb
@@ -12,30 +12,18 @@ module Mogura
     DIG_EXT = '.dig'.freeze
 
     class << self
-      def push(project: Rails.application.class.module_parent_name, dags: {})
+      def push(project: Rails.application.class.module_parent_name, dags: [])
         upload(gzip(tar(dags)), Mogura.config.endpoint, project, revision)
       end
 
       private
 
-      def export
-        {
-          "_export": {
-            "rb": {
-              "require": Rails.root.join('config/environment').to_s
-            }
-          }
-        }.freeze
-      end
-
       def tar(dags)
         tarfile = StringIO.new("")
         Gem::Package::TarWriter.new(tarfile) do |tar|
-          dags.each do |dag_name, dag_content|
-            puts "dag_name: #{dag_name}"
-            puts "dag_content: #{dag_content}"
-            tar.add_file "#{dag_name}#{DIG_EXT}", 33188 do |tf|
-              tf.write JSON.pretty_generate(export.merge(dag_content))
+          dags.each do |dag|
+            tar.add_file "#{dag.name}#{DIG_EXT}", 33188 do |tf|
+              tf.write JSON.pretty_generate(dag.content)
             end
           end
         end
