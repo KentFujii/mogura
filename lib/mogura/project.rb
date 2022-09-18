2d4f410ff5f714d02a6126348a23dfcf3e751eea
KentFujii <kent.where.the.light.is@gmail.com>
2022-09-18 21:34:20 +0900

    fix

--- lib/mogura/project.rb
+++ lib/mogura/project.rb
@@ -1,10 +1,80 @@
+require 'rails'
+require 'rubygems'
+require 'rubygems/package'
+require 'zlib'
+require 'fileutils'
+require 'net/http'
+require 'uri'
+
 module Mogura
-  class Project
-    class << self
-      def projects
-        uri = URI.parse("#{Mogura.config.endpoint}/api/projects")
-        response = Net::HTTP.get_response(uri)
-        JSON.parse(response.body)
+  module Project
+    class Put
+      class << self
+        FILE_MODE = 33188
+        DIG_EXT = '.dig'.freeze
+
+        def project(project: Rails.application.class.module_parent_name, dags: [])
+          upload(gzip(tar(dags)), Mogura.config.endpoint, project, revision)
+        end
+
+        def secret
+          # https://docs.digdag.io/api/
+          raise NotImplementedError, "You must implement #{self.name}##{__method__}"
+        end
+
+        private
+
+        def tar(dags)
+          tarfile = StringIO.new("")
+          Gem::Package::TarWriter.new(tarfile) do |tar|
+            dags.each do |dag|
+              tar.add_file "#{dag.name}#{DIG_EXT}", FILE_MODE do |tf|
+                tf.write JSON.pretty_generate(dag.content)
+              end
+            end
+          end
+          tarfile
+        end
+
+        def gzip(tarfile)
+          gz = StringIO.new("")
+          z = Zlib::GzipWriter.new(gz)
+          z.write tarfile.string
+          z.close
+          StringIO.new(gz.string, binmode: true)
+        end
+
+        def revision
+          SecureRandom.hex(10)
+        end
+
+        def upload(gzip, endpoint, project, revision)
+          uri = URI.parse("#{endpoint}/api/projects?project=#{project}&revision=#{revision}")
+          request = Net::HTTP::Put.new(uri)
+          request.content_type = "application/gzip"
+          request.body = gzip.read
+          req_options = {
+            use_ssl: uri.scheme == "https",
+          }
+          Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+            http.request(request)
+          end
+        end
+      end
+    end
+    
+    class Get
+      class << self
+        def projects
+          uri = URI.parse("#{Mogura.config.endpoint}/api/projects")
+          response = Net::HTTP.get_response(uri)
+          JSON.parse(response.body)
+        end
+
+        def project
+          # https://docs.digdag.io/api/
+          raise NotImplementedError, "You must implement #{self.name}##{__method__}"
+        end
       end
     end
   end
